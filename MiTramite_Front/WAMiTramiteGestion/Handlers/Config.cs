7a3c47817b8d5186b903ec7cdb8eb6cea9a63434
Bryan <bryangod194@gmail.com>
2025-11-04 00:53:59 -0400

    Adicion de archivo de configuracion base en ambos proyectos

---
+++ MiTramite_Front/WAMiTramiteGestion/Handlers/Config.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WAMiTramiteGestion.Handlers
+{
+    public static class Config
+    {
+        public static string ApiUrl { get; set; } = "http://localhost:5080/";
+        public static int Timeout { get; set; } = 30;
+
+
+    }
+}
