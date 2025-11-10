9e9305a50f1afe8b83818e12e4bf4ef4c91ce0de
BiMq0 <bryangod194@gmail.com>
2025-11-10 16:09:39 -0400

    adicion de middleware con solicitud de tokens

--- MiTramite_Back/Program.cs
+++ MiTramite_Back/Program.cs
@@ -2,22 +2,13 @@ using MiTramite_Back.Handlers;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 using MiTramite_Back.Acceso_A_Datos.Context;
+using MiTramite_Back.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
 //Adicion de Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo { Title = "MiTramite API", Description = "Documentacion de API para MiTramite", Version = "v1" }); });
-builder.Services.AddDistributedMemoryCache();
-builder.Services.AddSession(options =>
-{
-   options.IdleTimeout = TimeSpan.FromMinutes(480);
-   options.Cookie.HttpOnly = true;
-   options.Cookie.IsEssential = true;
-});
-
-builder.Services.AddHttpContextAccessor(); // Necesario para acceder a HttpContext en servicios
-
 
 //Database
 builder.Services.AddDbContext<MiTramiteDbContext>(options =>
@@ -25,8 +16,10 @@ builder.Services.AddDbContext<MiTramiteDbContext>(options =>
    options.UseNpgsql(builder.Configuration.GetConnectionString("DatabaseConnectionString"));
 });
 
+// JWT Configuration
+builder.Services.Configure<JwtOptions>(builder.Configuration.GetSection("Jwt"));
 
-//Scopes
+// Scopes (esto registra automáticamente ITokenService y TokenService)
 builder.Services.AddScopedRepositories();
 builder.Services.AddScopedServices();
 
@@ -40,7 +33,8 @@ if (app.Environment.IsDevelopment())
       c.SwaggerEndpoint("/swagger/v1/swagger.json", "PharmAPI V1");
    });
 }
-app.UseSession();
+
+app.AddMiddleware();
 app.MapEndpoints();
 app.MapGet("/", () => Results.Redirect("/swagger"));
 app.Run();
